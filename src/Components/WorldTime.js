5d77c716fb72dff4b151c8fc6fef691c31ef695d
lonelybuddy <henryzheng92@gmail.com>
2021-12-20 13:42:38 -0600

    styled WorldTime component

--- src/Components/WorldTime.js
+++ src/Components/WorldTime.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './WorldTime.Style.scss';
+// import Sun from '../Assets/desktop/icon-sun.svg';
+import Moon from '../Assets/desktop/icon-moon.svg';
 
 const WorldTime = () => {
     return (
-        <section className="wold-time">
-            <p className="word-time__greeting">GOOD EVENING</p>
+        <section className="world-time">
+            <p className="world-time__greeting">
+                <img className="world-time__greeting-icon" src={Moon} alt="" />
+                GOOD EVENING
+            </p>
+
+            <div>
+                <span className="world-time__time">23:14</span>
+                <span className="world-time__time-zone">BST</span>
+            </div>
+
+            <p className="world-time__location">IN LONDON, UK</p>
         </section>
     );
 };
