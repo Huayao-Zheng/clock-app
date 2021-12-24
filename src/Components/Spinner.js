8f94c12243d22055789759d3352e1824ea46ac43
lonelybuddy <henryzheng92@gmail.com>
2021-12-23 20:19:29 -0600

    added message to spinner when the client's Adblocker blocks API calls

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './Spinner.Style.scss';
 
 const Spinner = () => {
-    return <div className="spinner"></div>;
+    return (
+        <>
+            <div className="spinner"></div>
+            <div className="turn-off-ad-blocker">If It's still loading, try turn off the Adblocker.</div>
+        </>
+    );
 };
 
 export default Spinner;
